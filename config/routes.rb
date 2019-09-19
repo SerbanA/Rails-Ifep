514446d3ac7d054f62a2a3259cbb50bc069d02b4
SerbanA <adyserban97@yahoo.com>
2019-09-19 15:29:35 +0300

    error when testing validation, points to gsub

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'admin#verifylawyer' 
   get 'admin/verifylawyer' => 'admin#verifylawyers'
-  post 'search', to: 'admin#search', as: 'admin/verifylawyers'
+  post 'search', to: 'admin#search', as: 'admin/search'
+
+  post 'is_valid', to: 'admin#is_valid', as: 'admin/is_valid'
+  post 'not_valid', to: 'admin#not_valid', as: 'admin/not_valid'
   resources :job, :name, :state, :phone, :email
  
 
