a908519ec855cfe07876e57203298e5a77654222
SerbanA <adyserban97@yahoo.com>
2019-10-10 14:56:18 +0300

    modified search_page to get method(wip)

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,14 +10,18 @@ class AdminController < ApplicationController
         command = Ifep::MainProgram.call(Ifep::Filters.headers, Ifep::Filters.form["body"], params[:search_value])
         @search_value = params[:search_value]
         @lawyer = command.result
+        @status = params[:commit]
     end
 
     def validate_lawyer
-        if lawyer_ok?
-            p "This lawyer's data is OK and will be submitted to /api/lawyer/#{@UUID}/verify/#{params[:commit]}"
+        @status = params[:commit]
+        message = if lawyer_ok?
+            "This lawyer's data is OK and will be submitted to /api/lawyer/#{@UUID}/verify/#{params[:commit]}"
         else 
-            p "This lawyer's data is NOT-OK and will be submitted to /api/lawyer/#{@UUID}/verify/#{params[:commit]}"
+            "This lawyer's data is NOT-OK and will be submitted to /api/lawyer/#{@UUID}/verify/#{params[:commit]}"
         end
+
+        render html: message
     end
     
     private
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get  "admin/search_page"
   root 'admin#search_page' 
-
-  resources :uuid,:search_value
   
-  post "/api/search/UUID/:uuid" => 'admin#search_results',as: 'admin_search_results'
-  post "/api/UUID/:uuid/search_value/:search_value" => "admin#validate_lawyer", as: 'admin_validate_lawyer'
+  get "api/search/:search_value/:uuid" => 'admin#search_results', as: 'admin_search_results'
+  post "api/verify/:search_value/:uuid/verify/" => "admin#validate_lawyer", as: 'admin_validate_lawyer'
 
 end
