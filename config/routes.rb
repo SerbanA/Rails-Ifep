2c7cc517d719555311edc97571f6b997741b07bc
SerbanA <adyserban97@yahoo.com>
2019-10-14 13:56:31 +0300

    finished separation of concern

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'admin#search_page' 
-  get  "api/search/:search_value/:uuid" => 'admin#search_results', as: 'admin_search_results'
-  post "api/lawyer/:search_value/:uuid/verify/" => "admin#validate_lawyer", as: 'admin_validate_lawyer'
+  get  "/search/form" => 'admin#search_page'
+
+  get  "/search" => 'admin#search_results'
+  get  "/lawyers/:UUID/:search_value" => 'admin#search_results', as: 'admin_search_results'
+  post "api/lawyer/:search_value/:UUID/verify/" => "admin#validate_lawyer", as: 'admin_validate_lawyer'
 
 end
